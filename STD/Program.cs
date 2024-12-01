f30dcd46cdd12aa7046667186f211a533620e322
maior <maior@DESKTOP-FHGLER9>
2024-12-01 18:48:04 +0200

    Добавьте файлы проекта.

---
+++ STD/Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+
+class Program
+{
+    static void Main()
+    {
+        Console.WriteLine("Введите текст:");
+        string input = Console.ReadLine();
+        string[] words = input.Split(new char[] { ' ', ',', '.', '!', '?' }, StringSplitOptions.RemoveEmptyEntries);
+
+        if (words.Length == 0)
+        {
+            Console.WriteLine("Нет слов в введённом тексте.");
+            return;
+        }
+
+        int minLength = int.MaxValue;
+        int maxLength = int.MinValue;
+
+        foreach (string word in words)
+        {
+            int length = word.Length;
+            if (length < minLength)
+            {
+                minLength = length;
+            }
+            if (length > maxLength)
+            {
+                maxLength = length;
+            }
+        }
+
+        Console.WriteLine($"Длина самого короткого слова: {minLength}");
+        Console.WriteLine($"Длина самого длинного слова: {maxLength}");
+    }
+}
